8546dd40c15f9744b84df43eeb9414f52ea2da8d
alireza ashrafi <alirezaashrafi.email@gmail.com>
2018-03-01 02:04:59 +0330

    fork init

---
+++ searchdialog/src/main/java/com/alirezaashrafi/searchdialog/OnSearchItemSelected.java
@@ -0,0 +1,9 @@
+package com.alirezaashrafi.searchdialog;
+
+/**
+ * Created by alirezaashrafi on 06/11/17.
+ */
+
+public interface OnSearchItemSelected {
+    void onClick(int position, SearchListItem searchListItem);
+}
